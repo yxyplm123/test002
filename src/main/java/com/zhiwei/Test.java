a0b009e3a8e1eaf453849c5485e948587457290d
yanxinyu <2425453191@qq.com>
2020-07-02 11:24:40 +0800

    测试

--- src/main/java/com/zhiwei/Test.java
+++ src/main/java/com/zhiwei/Test.java
@@ -9,5 +9,18 @@ public class Test {
         System.out.println("123456");
         System.out.println("你好阿，北京");
         System.out.println("我晓得了");
+
+         String  a = "012";
+        String substring = a.substring(1);
+        String month = substring.concat("月");
+        System.out.println(month);
+        String space = month.substring(0, month.lastIndexOf("月"));
+        System.out.println(space);
+        int b = 5;
+        if(b ==7 ){
+            System.out.println("flase");
+        }else {
+            System.out.println("true");
+        }
     }
 }
